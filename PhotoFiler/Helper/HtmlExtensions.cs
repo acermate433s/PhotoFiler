68ee339f821e25ac6afffb2dfa4903491db028c2
Adrian Demavivas <adrian.demavivas@gmail.com>
2016-03-12 10:12:13 -0600

    - Added bottom padding span enclosing gallery previews - Added class attribute to Image extension

--- PhotoFiler/Helper/HtmlExtensions.cs
+++ PhotoFiler/Helper/HtmlExtensions.cs
@@ -8,11 +8,15 @@ namespace PhotoFiler.Helper
 {
     public static class HtmlExtensions
     {
-        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText)
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, string className = "")
         {
             var builder = new TagBuilder("img");
             builder.MergeAttribute("src", src);
             builder.MergeAttribute("alt", altText);
+
+            if (className != "")
+                builder.MergeAttribute("class", className);
+
             return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
         }
     }
