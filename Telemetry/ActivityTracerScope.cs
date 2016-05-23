0cc045313ded99bbea84655e2b6d33120248a345
Adrian Demavivas <adrian.demavivas@gmail.com>
2016-05-23 09:43:19 -0500

    Initial commit for activity tracker - Added class ActivityTraceScope - Added class ActivityTraceTypeScope

---
+++ Telemetry/ActivityTracerScope.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Telemetry
+{
+    /// <summary>
+    /// Base on http://www.codeproject.com/Articles/185666/ActivityTracerScope-Easy-activity-tracing-with
+    /// </summary>
+    public class ActivityTracerScope : IDisposable
+    {
+        /// <summary>
+        /// Previous ID before TransferTrace
+        /// </summary>
+        protected Guid PreviousID { get; set; }
+
+        /// <summary>
+        /// ID for the current TransferTrace
+        /// </summary>
+        protected Guid CurrentID { get; set; }
+
+        /// <summary>
+        /// Current TraceSource 
+        /// </summary>
+        public TraceSource TraceSource { get; protected set; }
+
+        /// <summary>
+        /// User-defined ID for the activity
+        /// </summary>
+        public int ActivityID { get; protected set; }
+
+        /// <summary>
+        /// User-defined name for the activity
+        /// </summary>
+        public string ActivityName { get; protected set; }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="traceSource">TraceSource to use to log activity</param>
+        /// <param name="activityName">User-defined name for the current activity</param>
+        /// <param name="activityID">User-defined id for the current activity</param>
+        public ActivityTracerScope(
+            TraceSource traceSource,
+            string activityName = "",
+            int activityID = 0
+        ) 
+        {   
+            TraceSource = traceSource;
+            ActivityID = activityID;
+            ActivityName = activityName;
+
+            // remember the previous activity ID so we could come back to it 
+            // later when we switch back to the previous activity before this
+            PreviousID = Trace.CorrelationManager.ActivityId;
+
+            // create a new ID for the current activity; we would need this 
+            // when we when call TraceEvent with TraceEventType.Stop
+            CurrentID = Guid.NewGuid();
+
+            // transfer to a new activity and then start the trace event
+            if (PreviousID != Guid.Empty)
+                TraceSource.TraceTransfer(ActivityID, "Transferring to new activity", CurrentID);
+            Trace.CorrelationManager.ActivityId = CurrentID;
+            TraceSource.TraceEvent(TraceEventType.Start, ActivityID, ActivityName);
+        }
+
+        /// <summary>
+        /// Creates a new TraceSource instead of using a user-defined one.
+        /// </summary>
+        /// <param name="traceName">Name for the soon-to-be created TraceSource to use to log activity</param>
+        /// <param name="activityName">User-defined name for the current activity</param>
+        /// <param name="activityID">User-defined id for the current activity</param>
+        public ActivityTracerScope(
+            string traceName,
+            string activityName = "",
+            int activityID = 0
+        ) : this(new TraceSource(traceName), activityName, activityID)
+        {
+        }
+
+        /// <summary>
+        /// Transfer to the previous activity and then stop the current trace event
+        /// </summary>
+        public void Dispose()
+        {
+            if (PreviousID != Guid.Empty)
+                TraceSource.TraceTransfer(ActivityID, "Transferring back to previous activity", PreviousID);
+            TraceSource.TraceEvent(TraceEventType.Stop, ActivityID, ActivityName);
+            Trace.CorrelationManager.ActivityId = PreviousID;
+        }
+
+        #region " TraceSource method short cuts "
+
+        public void Data(TraceEventType eventType, object data)
+        {
+            TraceSource.TraceData(eventType, ActivityID, data);
+        }
+
+        public void Data(TraceEventType eventType, params object[] data)
+        {
+            TraceSource.TraceData(eventType, ActivityID, data);
+        }
+
+        private void Event(TraceEventType eventType)
+        {
+            TraceSource.TraceEvent(eventType, ActivityID);
+        }
+
+        private void Event(TraceEventType eventType, string message)
+        {
+            TraceSource.TraceEvent(eventType, ActivityID, message);
+        }
+
+        private void Event(TraceEventType eventType, string message, params object[] args)
+        {
+            TraceSource.TraceEvent(eventType, ActivityID, message, args);
+        }
+
+        public void Critical(string message)
+        {
+            Event(TraceEventType.Critical, message);
+        }
+
+        public void Critical(string message, params object[] args)
+        {
+            Event(TraceEventType.Critical, message, args);
+        }
+
+        public void Error(string message)
+        {
+            Event(TraceEventType.Error, message);
+        }
+
+        public void Error(string message, params object[] args)
+        {
+            Event(TraceEventType.Error, message, args);
+        }
+
+        public void Warning(string message)
+        {
+            Event(TraceEventType.Warning, message);
+        }
+
+        public void Warning(string message, params object[] args)
+        {
+            Event(TraceEventType.Warning, message, args);
+        }
+
+        public void Information(string message)
+        {
+            TraceSource.TraceInformation(message);
+        }
+
+        public void Information(string message, params object[] args)
+        {
+            TraceSource.TraceInformation(message, args);
+        }
+
+        public void Verbose(string message)
+        {
+            Event(TraceEventType.Verbose, message);
+        }
+
+        public void Verbose(string message, params object[] args)
+        {
+            Event(TraceEventType.Verbose, message, args);
+        }
+
+        #endregion
+    }
+}
---
+++ Telemetry/ActivityTracerTypeScope.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Telemetry
+{
+    /// <summary>
+    /// Base on http://www.codeproject.com/Articles/185666/ActivityTracerScope-Easy-activity-tracing-with
+    /// </summary>
+    public class ActivityTracerTypeScope<ActivityEnumType> : ActivityTracerScope where ActivityEnumType : struct, IConvertible, IComparable, IFormattable
+    {
+        public readonly ActivityEnumType Activity;
+
+        public new int ActivityID
+        {
+            get
+            {
+                return Activity.ToInt32(null);
+            }
+        }
+
+        public new string ActivityName
+        {
+            get
+            {
+                return GetDescription(Activity);
+            }
+        }
+
+        private static string GetDescription(
+            ActivityEnumType activityType
+        )
+        {
+            // default would be the ToString() method of the ActivityEnum
+            // if we cannot find a DescriptionAttribute
+            var result = activityType.ToString();
+
+            // get the DescriptionAttribute of the ActivityEnum and
+            // use the Description property as the return value
+            var fieldInfo = activityType.GetType().GetField(result);
+            var attributes = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+            if (attributes.Length > 0 && attributes[0] is DescriptionAttribute)
+            {
+                var description = (DescriptionAttribute) attributes[0];
+                result = description.Description;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="traceSource">TraceSource to use to log activity</param>
+        /// <param name="activityType">Activity type of the current activity</param>
+        public ActivityTracerTypeScope(
+            TraceSource traceSource,
+            ActivityEnumType activityType = default(ActivityEnumType)
+        ) : base(
+                traceSource, 
+                GetDescription(activityType), 
+                activityType.ToInt32(null)
+        )
+        {
+            if (!typeof(ActivityEnumType).IsEnum)
+                throw new NotSupportedException($"{nameof(ActivityEnumType)} must be an enumerated type");
+
+            Activity = activityType;
+
+            // remember the previous activity ID so we could come back to it 
+            // later when we switch back to the previous activity before this
+            PreviousID = Trace.CorrelationManager.ActivityId;
+
+            // create a new ID for the current activity; we would need this 
+            // when we when call TraceEvent with TraceEventType.Stop
+            CurrentID = Guid.NewGuid();
+
+            // transfer to a new activity and then start the trace event
+            if (PreviousID != Guid.Empty)
+                TraceSource.TraceTransfer(ActivityID, "Transferring to new activity", CurrentID);
+            Trace.CorrelationManager.ActivityId = CurrentID;
+            TraceSource.TraceEvent(TraceEventType.Start, ActivityID, ActivityName);
+        }
+
+        /// <summary>
+        /// Creates a new TraceSource instead of using a user-defined one.
+        /// </summary>
+        /// <param name="traceName">Activity name of the current activity</param>
+        /// <param name="activityType">Activity type of the current activity</param>
+        public ActivityTracerTypeScope(
+            string traceName,
+            ActivityEnumType activityType = default(ActivityEnumType)
+        ) : this(new TraceSource(traceName), activityType)
+        {
+        }
+    }
+}
