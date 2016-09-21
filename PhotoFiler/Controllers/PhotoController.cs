4bf7b03f674de58253db0af45809d718e7343a46
Adrian Demavivas <adrian.demavivas@gmail.com>
2016-09-20 21:54:00 -0500

    Removed logging in PhotoController

--- PhotoFiler/Controllers/PhotoController.cs
+++ PhotoFiler/Controllers/PhotoController.cs
@@ -13,7 +13,6 @@ namespace PhotoFiler.Controllers
         private const int DEFAULT_COUNT = 12;
 
         private IHashedAlbum Album = (IHashedAlbum) System.Web.HttpContext.Current.Application["Album"];
-        private ILogger Logger = (ILogger) System.Web.HttpContext.Current.Application["Logger"];
 
         public PhotoController()
         {
@@ -22,67 +21,47 @@ namespace PhotoFiler.Controllers
         [Route("{hash}")]
         public ActionResult Index(string hash)
         {
-            using (var logger = Logger.Create($"PhotoController.Index(hash = \"{hash}\")"))
+            if (hash != null)
             {
-                logger.Information(Request.Url.AbsoluteUri);
-
-                if (hash != null)
-                {
-                    logger.Information("Retrieve photo with hash \"{0}\".", hash);
-                    return Retrieve(hash, true);
-                }
-                else
-                {
-                    logger.Warning("Hash cannot be null.");
-                    return RedirectToAction("Index", "Home");
-                }
+                return Retrieve(hash, true);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
             }
         }
 
         [Route("Download/{hash}")]
         public ActionResult Download(string hash)
         {
-            using (var logger = Logger.Create($"PhotoController.Download(hash = \"{hash}\")"))
+            if (hash != null)
             {
-                logger.Information(Request.Url.AbsoluteUri);
-
-                if (hash != null)
-                {
-                    logger.Information("Retrieve photo with hash \"{0}\".", hash);
-                    return Retrieve(hash, false);
-                }
-                else
-                {
-                    logger.Warning("Hash cannot be null.");
-                    return RedirectToAction("Index", "Home");
-                }
+                return Retrieve(hash, false);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
             }
         }
 
         [Route("Preview/{hash}")]
         public ActionResult Preview(string hash)
         {
-            using (var logger = Logger.Create($"PhotoController.Preview(hash = \"{hash}\")"))
+            var content = Album.Preview(hash);
+            if (content == null)
             {
-                logger.Information(Request.Url.AbsoluteUri);
-
-                var content = Album.Preview(hash);
-                if (content == null)
-                {
-                    logger.Warning("Cannot find preview for photo with hash {0}", hash);
-                    return new HttpNotFoundResult($"Cannot find preview for photo with \"{hash}\"");
-                }
-
-                var result =
-                    ImageFile(
-                        hash,
-                        true,
-                        Album.Photo(hash),
-                        content
-                    );
-
-                return result;
+                return new HttpNotFoundResult($"Cannot find preview for photo with \"{hash}\"");
             }
+
+            var result =
+                ImageFile(
+                    hash,
+                    true,
+                    Album.Photo(hash),
+                    content
+                );
+
+            return result;
         }
 
         private ActionResult Retrieve(string hash, bool inline)
@@ -91,11 +70,11 @@ namespace PhotoFiler.Controllers
             if (content == null)
                 return new HttpNotFoundResult($"Cannot find view for \"{hash}\"");
 
-            var result = 
+            var result =
                 ImageFile(
-                    hash, 
-                    inline, 
-                    Album.Photo(hash), 
+                    hash,
+                    inline,
+                    Album.Photo(hash),
                     content
                 );
 
@@ -103,9 +82,9 @@ namespace PhotoFiler.Controllers
         }
 
         private ActionResult ImageFile(
-            string hash, 
-            bool inline, 
-            IPhoto photo, 
+            string hash,
+            bool inline,
+            IPhoto photo,
             byte[] content
         )
         {
@@ -129,41 +108,31 @@ namespace PhotoFiler.Controllers
         [Route("List")]
         public ActionResult List(int page = DEFAULT_PAGE, int count = DEFAULT_COUNT)
         {
-            using (var logger = Logger.Create($"PhotoController.List(page = {page}, count = {count})"))
-            {
-                int total = Album.Count();
+            int total = Album.Count();
 
-                ViewBag.Total = total;
-                ViewBag.Previous = page - 1;
-                ViewBag.Current = page;
-                ViewBag.Max = total / count + (total % count == 0 ? 0 : 1);
-                ViewBag.Next = page + 1;
-                ViewBag.Count = count;
-            
-                logger.Information(Request.Url.AbsoluteUri);
+            ViewBag.Total = total;
+            ViewBag.Previous = page - 1;
+            ViewBag.Current = page;
+            ViewBag.Max = total / count + (total % count == 0 ? 0 : 1);
+            ViewBag.Next = page + 1;
+            ViewBag.Count = count;
 
-                return View(Album.List(page, count));
-            }
+            return View(Album.List(page, count));
         }
 
         [Route("")]
         public ActionResult Gallery(int page = DEFAULT_PAGE, int count = DEFAULT_COUNT)
         {
-            using (var logger = Logger.Create($"PhotoController.Gallery(page = {page}, count = {count}"))
-            {
-                int total = Album.Count();
-                    
-                ViewBag.Total = total;
-                ViewBag.Previous = page - 1;
-                ViewBag.Current = page;
-                ViewBag.Max = total / count + (total % count == 0 ? 0 : 1);
-                ViewBag.Next = page + 1;
-                ViewBag.Count = count;
-            
-                logger.Information(Request.Url.AbsoluteUri);
-                    
-                return View(Album.List(page, count));
-            }
+            int total = Album.Count();
+
+            ViewBag.Total = total;
+            ViewBag.Previous = page - 1;
+            ViewBag.Current = page;
+            ViewBag.Max = total / count + (total % count == 0 ? 0 : 1);
+            ViewBag.Next = page + 1;
+            ViewBag.Count = count;
+
+            return View(Album.List(page, count));
         }
     }
 }
